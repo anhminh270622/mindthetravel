fd39c3fc942786f7d740df3edc1252807c7c03cf
anhminh270622 <20020444@vnu.edu.vn>
2024-10-24 01:55:56 +0700

    Update: sibar and backTop

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,8 @@ import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import HeaderScreen from '../web/features/header/screen/HeaderScreen'
 import FooterScreen from '../web/features/footer/footerScreen/FooterScreen'
+import ScrollToTop from '../web/features/scrollToTop/ScrollToTop'
+import SideBar from '../web/features/sideBar/SideBar'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -14,6 +16,8 @@ function RootComponent() {
       <HeaderScreen />
       <Outlet />
       <FooterScreen />
+      <ScrollToTop />
+      <SideBar />
       <TanStackRouterDevtools position="bottom-right" />
     </div>
   )
---
+++ src/web/features/scrollToTop/ScrollToTop.tsx
@@ -0,0 +1,24 @@
+import { UpOutlined } from '@ant-design/icons'
+import { BackTop, Image } from 'antd'
+import React from 'react'
+
+export default function ScrollToTop() {
+    return (
+        <BackTop visibilityHeight={500}>
+            <Image
+                preview={false}
+                src="https://mindthetravel.com/wp-content/uploads/2016/05/up-arrow.png"
+                style={{
+                    backgroundColor: "#9fa08f",
+                    padding: "5px",
+                    borderRadius: "50%",
+                    color: "white",
+                    position: "fixed",
+                    bottom: "50px",
+                    right: "20px",
+                    width: "44px",
+                    height: "44px"
+                }} />
+        </BackTop>
+    )
+}
